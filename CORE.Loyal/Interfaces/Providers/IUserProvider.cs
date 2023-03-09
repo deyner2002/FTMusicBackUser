c323b43a87966c49212252eab058b26bb0434956
deyner2002 <solano27deiner@gmail.com>
2023-03-09 15:49:54 -0500

    first commit

---
+++ CORE.Loyal/Interfaces/Providers/IUserProvider.cs
@@ -0,0 +1,10 @@
+﻿using Core.Loyal.Models.FTMUSIC;
+
+namespace CORE.Loyal.Interfaces.Providers
+{
+    public interface IUserProvider
+    {
+        Task<List<UserModel>> GetList();
+        Task<long> SaveUser(UserModel user);
+    }
+}
