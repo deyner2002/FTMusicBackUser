173be51952ae8552dd271cd864ee58140d130e33
BreinerAndresJaimesMiranda <breinerandresjaimesmiranda@gmail.com>
2023-03-13 13:29:13 -0500

    Implementacion de los required en el modelo de usuario

--- CORE.Loyal/Models/FTMUSIC/UsuarioModel.cs
+++ CORE.Loyal/Models/FTMUSIC/UsuarioModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace CORE.Loyal.Models.FTMUSIC
     public class UsuarioModel
     {
         public long? Id { get; set; }
+
+        [Required(ErrorMessage = "El Nombre del usuario es requerido")]
         public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El Correo del usuario es requerido")]
         public string? Correo { get; set; }
+        [Required(ErrorMessage = "La Contrasenia del usuario es requerida")]
         public string? Contrasenia { get; set; }
         public DateTime? FechaRegistro { get; set; }
+        [Required(ErrorMessage = "La descripcion del usuario es requerida")]
         public string? Descripcion { get; set; }
         public DateTime? FechaNacimiento { get; set; }
         public string? Facebook { get; set; }
